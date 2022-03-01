21858d2647f101010bef4ff45c74d649e063c256
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2022-03-01 07:28:47 +0700

    Add files via upload

---
+++ Java1/Interface/MovablePoint.java
@@ -0,0 +1,35 @@
+package Interface;
+
+public class MovablePoint implements Movable{
+	private int x, y;
+
+	@Override
+	public void moveUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		return "MovablePoint [x=" + x + ", y=" + y + "]";
+	}
+	
+}
